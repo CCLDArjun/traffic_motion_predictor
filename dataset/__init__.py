ef69a91b5b8340b21e9e030c28ef6f5f1a17aa44
CCLDArjun <ccldarjun@icloud.com>
2024-02-16 17:19:24 -0800

    add nuscenes dev kit, setup basic pytorch dataset interface

---
+++ dataset/__init__.py
@@ -0,0 +1,2 @@
+from .nuscenes import NuScenesDataset
+
--- dataset/nuscenes.py
+++ dataset/nuscenes.py
@@ -1,15 +1,69 @@
 from torch.utils.data import Dataset
+from nuscenes import NuScenes
+from nuscenes.eval.prediction.splits import get_prediction_challenge_split
+from nuscenes.prediction import PredictHelper
 
+_size_to_version = {
+    "mini": "v1.0-mini",
+    "full": "v1.0-trainval"
+}
 
-class NuScenes(Dataset):
-    def __init__(self, dataroot):
+_size_to_split = {
+    "mini": "mini_train",
+    "full": "trainval"
+}
+
+class NuScenesDataset(Dataset):
+    def __init__(self, dataroot, size="mini", seconds_in_future=3):
+        self.nusc = NuScenes(version=_size_to_version[size], dataroot=dataroot)
+        self.instances = get_prediction_challenge_split(_size_to_split[size], dataroot=dataroot)
         self.dataroot = dataroot
+        self.helper = PredictHelper(self.nusc)
+        self.seconds_in_future = seconds_in_future
 
     def __len__(self):
-        # TODO (not that important)
-        return 0
+        return len(self.instances)
 
     def __getitem__(self, idx):
-        # TODO: return a sample, do the filtering here
-        return data
+        # instance_token  is the token of the agent we want to track
+        # sample_token is the token of the sample (video scenario) we want to track the agent in
+        instance_token, sample_token = mini_train[idx].split("_")
+        sample = self.helper.get_sample_annotation(instance_token, sample_token)
+        kwargs = {
+            "instance_token": instance_token,
+            "sample_token": sample_token,
+            "seconds_in_future": self.seconds_in_future,
+        }
+
+        agent_future_xy_local = self.helper.get_future_for_agent(
+            **kwargs,
+            in_agent_frame=True,
+        )
+
+        agent_future_xy_global = self.helper.get_future_for_agent(
+            **kwargs,
+            in_agent_frame=False,
+        )
+
+        agent_past_xy_local = self.helper.get_past_for_agent(
+            **kwargs,
+            in_agent_frame=True,
+        )
+
+        global_annotations = self.helper.get_annotations_for_sample(instance_token, sample_token)
+
+        return {
+            "global_annotations": global_annotations,
+            "annotation": annotation,
+            "instance_token": instance_token,
+            "sample_token": sample_token,
+            "future": {
+                "agent_xy_local": agent_future_xy_local,
+                "agent_xy_global": agent_future_xy_global,
+            },
+            "past": {
+                "agent_xy_local": agent_past_xy_local,
+                "agent_xy_global": agent_past_xy_global,
+            },
+        }
 
