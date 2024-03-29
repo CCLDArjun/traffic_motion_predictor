d9bb5e5a04d61475136d026bf5f3cf0fead34651
arjun <ccldarjun@icloud.com>
2024-03-29 00:07:38 -0700

    show images

--- programs/visualizer.py
+++ programs/visualizer.py
@@ -5,14 +5,24 @@ import torch
 import matplotlib.pyplot as plt
 
 from dataset import ChunkReader
+from dataset.simple_cnn_dataset import denormalize_xy, normalize
 import random
 
 d = ChunkReader("./chunks/")
 
 # choose a random data point
-x = random.randrange(0, len(d))
-sample = d[x]
+x = 4000
+raster_image, _, gt_xy  = d[x]
 
 # plot the raster image
+raster_image = raster_image.permute(0, 2, 3, 1).squeeze() / 255.
+plt.imshow(raster_image)
 
+# plot the target trajectory
+
+gt_xy *= 500
+plt.plot(gt_xy[:, 0], gt_xy[:, 1], 'ro')
+
+print(gt_xy)
+plt.savefig("raster_image.png")
 
