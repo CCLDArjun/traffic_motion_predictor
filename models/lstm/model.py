03f5f5cf030fa8d4951b7f2824646ecfdcb3fad8
arjun <ccldarjun@icloud.com>
2024-04-09 19:07:52 -0700

    initial lstm stuff

--- models/lstm/model.py
+++ models/lstm/model.py
@@ -5,17 +5,33 @@ import torch.nn.functional as F
 from simple_cnn import SimpleCNN
 
 class LSTMPredictor(torch.nn.Module):
-    def __init__(self, backbone_cnn, backbone_cnn_output_len):
+    def __init__(self, traffic_encoder, traffic_encoder_output_len, hidden_size=128):
         super(LSTMPredictor, self).__init__()
-        self.backbone_cnn = backbone_cnn
-        self.backbone_cnn_output_len = backbone_cnn_output_len
-        self.lstm = torch.nn.LSTM(input_size=backbone_cnn_output_len, hidden_size=128, num_layers=2, batch_first=True)
-        self.fc = torch.nn.Linear(128, num_modes * (2 * predictions_per_mode + 1))
+        self.traffic_encoder = traffic_encoder
+        self.traffic_encoder_output_len = traffic_encoder_output_len
+        self.lstm = torch.nn.LSTM(
+            input_size=traffic_encoder_output_len,
+            hidden_size=hidden_size,
+            num_layers=2,
+            batch_first=True
+        )
 
-    def forward(self, raster_image, state_vector):
-        latent_information = self.backbone_cnn(raster_image, state_vector, latent_output="fc1")
-        x, _ = self.lstm(predictions)
-        x = self.fc(x)
-        return x, probabilities
+        self.fc_out = torch.nn.Linear(hidden_size, 2 * num_modes)
+        self.fc_hidden_in = torch.nn.Linear(traffic_encoder_output_len, hidden_size)
+        self.fc_input_in = torch.nn.Linear(traffic_encoder_output_len, hidden_size)
 
+    def forward(self, raster_image, state_vector, points_to_predict=12):
+        predictions = torch.empty((points_to_predict, 2 * num_modes))
+
+        encoded = self.traffic_encoder(raster_image, state_vector, latent_output="fc1")
+        hidden = self.fc_in(encoded)
+        cell = torch.zeros_like(hidden)
+
+        for i in range(points_to_predict):
+            output, (hidden, cell) = self.lstm(, (hidden, cell))
+            output = self.fc_out(output)
+
+            predictions[i] = output
+
+        return predictions
 
